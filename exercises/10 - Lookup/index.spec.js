70cb954ed51722cf1b3edda2a1ededac1abe8737
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-08-04 14:17:48 +0200

    test lookup

--- exercises/10 - Lookup/index.spec.js
+++ exercises/10 - Lookup/index.spec.js
@@ -1,59 +1,63 @@
 // Data
 const users = [
   {
-    login: 'knuth',
-    firstName: 'Donald',
-    lastName: 'Knuth',
-    likes: ['C', 'Unix'],
+    login: "knuth",
+    firstName: "Donald",
+    lastName: "Knuth",
+    likes: ["C", "Unix"],
   },
   {
-    login: 'norvig',
-    firstName: 'Peter',
-    lastName: 'Norvig',
-    likes: ['AI', 'Search', 'NASA', 'Mars'],
+    login: "norvig",
+    firstName: "Peter",
+    lastName: "Norvig",
+    likes: ["AI", "Search", "NASA", "Mars"],
   },
   {
-    login: 'mfowler',
-    firstName: 'Martin',
-    lastName: 'Fowler',
-    likes: ['Design Patterns', 'Refactoring'],
+    login: "mfowler",
+    firstName: "Martin",
+    lastName: "Fowler",
+    likes: ["Design Patterns", "Refactoring"],
   },
   {
-    login: 'kent',
-    firstName: 'Kent',
-    lastName: 'Beck',
-    likes: ['TDD', 'wikis', 'Design Patterns'],
+    login: "kent",
+    firstName: "Kent",
+    lastName: "Beck",
+    likes: ["TDD", "wikis", "Design Patterns"],
   },
 ];
 
 // lookup()
 const lookup = (login, property) => {
   // START -- THIS IS WHERE YOUR CODE GOES
+  const userFound = users.find((user) => user.login === login);
+  if (!userFound) throw new Error("/Could not find user/");
+  if (!userFound[property]) throw new Error("/Could not find property/");
+  return userFound[property];
   // END
 };
 
 // Tests
-describe('lookup()', () => {
+describe("lookup()", () => {
   it("lookup(<login>, 'likes') should return likes for the specified user.", () => {
-    const actual = lookup('norvig', 'likes');
-    const expected = ['AI', 'Search', 'NASA', 'Mars'];
+    const actual = lookup("norvig", "likes");
+    const expected = ["AI", "Search", "NASA", "Mars"];
 
     expect(actual).toEqual(expected);
   });
   it("lookup(<login>, 'lastName') should return the last name for the specified user", () => {
-    const actual = lookup('knuth', 'lastName');
-    const expected = 'Knuth';
+    const actual = lookup("knuth", "lastName");
+    const expected = "Knuth";
 
     expect(actual).toEqual(expected);
   });
-  it('with unknown user should throw an error with the correct message', () => {
+  it("with unknown user should throw an error with the correct message", () => {
     expect(() => {
-      lookup('nobody', 'likes');
+      lookup("nobody", "likes");
     }).toThrow(/Could not find user/);
   });
-  it('with unknown property should throw an error the correct message', () => {
+  it("with unknown property should throw an error the correct message", () => {
     expect(() => {
-      lookup('mfowler', 'noprop');
+      lookup("mfowler", "noprop");
     }).toThrow(/Could not find property/);
   });
 });
