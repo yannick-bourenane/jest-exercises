928db0039737062bb41c2c26bd1461694ec3cea9
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-08-04 14:55:58 +0200

    Component Testing

--- exercises/13 - Component Testing/index.js
+++ exercises/13 - Component Testing/index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default () => {
+export default ({ data, onClick }) => {
   // Implement this component to pass the tests in ./__tests/index.spec.js
+  return (
+    <ul>
+      {data.map((item) => (
+        <li
+          className={item.selected ? "selected" : ""}
+          onClick={() => onClick(item.key)}
+          key={item.key}
+        >
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
 };
