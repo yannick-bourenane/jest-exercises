80d2ced2d2123e20ee664fc755331fff6bdbbd9a
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-08-05 17:46:16 +0200

    Promise start

--- exercises/14 - Promise Testing/index.js
+++ exercises/14 - Promise Testing/index.js
@@ -1,37 +1,53 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export const LOCATION_ORIGIN = (() => {
-  if (typeof window !== 'undefined' && window.location !== 'undefined') {
-    const url = window.location.origin ||
+  if (typeof window !== "undefined" && window.location !== "undefined") {
+    const url =
+      window.location.origin ||
       window.location.protocol +
-        '//' +
+        "//" +
         window.location.hostname +
-        (window.location.port ? ':' + window.location.port : '');
-    if (url.toString() !== 'null') {
+        (window.location.port ? ":" + window.location.port : "");
+    if (url.toString() !== "null") {
       return url;
     }
   }
-  return 'http://localhost';
+  return "http://localhost";
 })();
 
 export const defaultFetchHeaders = {
   compress: false,
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    Accept: "application/json",
+    "Content-Type": "application/json",
   },
 };
 
-export const camelCase = input => {
+export const camelCase = (input) => {
   // TODO: implement
 };
 
-const normalizeCasing = value => {
+const normalizeCasing = (value) => {
   // TODO: implement
+  return value;
 };
 
-const callApi = (url = '', options = {}) => {
+const callApi = (url = "", options = {}) => {
   // TODO: implement
+  const apiUrl = /https?:\/\//.test(url) ? url : `${LOCATION_ORIGIN}${url}`;
+  const fetchOptions = Object.assign({}, defaultFetchHeaders, options);
+
+  return fetch(apiUrl, fetchOptions).then((resp) => {
+    if (resp.status !== 204) {
+      return resp.json().then((json) => {
+        const results = { json: normalizeCasing(json), resp };
+        return resp.status >= 500 && resp.status < 600
+          ? Promise.reject(results)
+          : results;
+      });
+    }
+    return { json: null, resp };
+  });
 };
 
 export default callApi;
