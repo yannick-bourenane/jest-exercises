207fcf9e72d78bbbf0b3c44639f4967848bb0664
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-08-06 10:54:17 +0200

    extend tests

--- exercises/15 - Expect Extensions/index.spec.js
+++ exercises/15 - Expect Extensions/index.spec.js
@@ -1,47 +1,55 @@
 expect.extend({
   toBeFunction(received) {
-    const pass = typeof received === 'function';
+    const pass = typeof received === "function";
     if (pass) {
       return {
         pass,
-        message: () => `expected value to not be a function; actual: ${typeof received}`,
+        message: () =>
+          `expected value to not be a function; actual: ${typeof received}`,
       };
     }
     return {
       pass: false,
-      message: () => `expected value to be a function; actual: ${typeof received}`,
+      message: () =>
+        `expected value to be a function; actual: ${typeof received}`,
     };
   },
   toHaveProperty(received, argument) {
-    const pass = typeof received[argument] !== 'undefined';
+    const pass = typeof received[argument] !== "undefined";
     if (pass) {
       return {
         pass,
-        message: () => `expected object to not have property: ${argument}; actual: ${Object.keys(received)}`,
+        message: () =>
+          `expected object to not have property: ${argument}; actual: ${Object.keys(
+            received
+          )}`,
       };
     }
     return {
       pass: false,
-      message: () => `expected object to be have property: ${argument}; actual: ${Object.keys(received)}`,
+      message: () =>
+        `expected object to be have property: ${argument}; actual: ${Object.keys(
+          received
+        )}`,
     };
   },
 });
 
-test('should return function', () => {
-  const value = undefined;
+test("should return function", () => {
+  const value = () => null;
   expect(value).toBeFunction();
 });
 
-test('should not be function', () => {
-  const value = undefined;
+test("should not be function", () => {
+  const value = "test";
   expect(value).not.toBeFunction();
 });
 
-test('should return property', () => {
-  const value = undefined;
-  expect(value).toHaveProperty('name');
+test("should return property", () => {
+  const value = { name: "toto" };
+  expect(value).toHaveProperty("name");
 });
-test('should return property', () => {
-  const value = undefined;
-  expect(value).not.toHaveProperty('age');
+test("should return property", () => {
+  const value = { name: 25 };
+  expect(value).not.toHaveProperty("age");
 });
